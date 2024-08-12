3a0a74efa8a57381f623fdb24934455d172c1f88
Tanushri Barsainya <125864601+tanushri1506@users.noreply.github.com>
2024-08-12 23:10:17 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const PORT = process.env.PORT || 7000;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -53,6 +55,6 @@ app.delete('/flashcards/:id', (req, res) => {
     });
 });
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
+app.listen(PORT, () => {
+    console.log(`Server running on port :${PORT}`);
 });
